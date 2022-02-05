55f2ca282d076a25cb387b626e4b5752e7c5d9a8
Azeezjim <azeexjim@gmail.com>
2022-02-05 00:30:10 -0800

    5th  commit

--- src/Components/header/styles/header.js
+++ src/Components/header/styles/header.js
@@ -230,9 +230,9 @@ export const PlayButton = styled.button`
   max-width: 130px;
   font-weight: bold;
   font-size: 20px;
-  margin-top: 10px;
+  margin-top: 30px;
   cursor: pointer;
-  transition: background-color 0.5s ease;
+  transition: background-color 1s ease;
 
   &:hover {
     background-color: #ff1e1e;
