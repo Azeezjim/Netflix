d0d01769dd510efca6429437f2de3312ad21ebdb
Azeezjim <azeexjim@gmail.com>
2022-02-08 06:04:23 -0800

    2nd  commit

--- src/Containers/browse.js
+++ src/Containers/browse.js
@@ -88,6 +88,10 @@ export default function  BrowsContainer({ slides }) {
                             {slideItem.data.map((item) => (
                                 <Card.Item key={item.docId} item={item}>
                                     <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}/>
+                                    <Card.Meta>
+                                        <Card.SubTitle>{item.title}</Card.SubTitle>
+                                        <Card.Text>{item.Text}</Card.Text>
+                                    </Card.Meta>
                                 </Card.Item>
                             ))}
                     </Card.Entities>
