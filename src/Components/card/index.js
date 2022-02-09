ce8ea6681e95ec91ce575ecfe84c508b88fbf09d
Azeezjim <azeexjim@gmail.com>
2022-02-09 07:31:33 -0800

    2nd  commit

--- src/Components/card/index.js
+++ src/Components/card/index.js
@@ -54,7 +54,7 @@ Card.Feature = function CardFeature ({children, catigory, ...restProps}) {
             <FeatureText>{itemFeature.description}</FeatureText>
             <FeatureClose onClick={() => setShowFeature(false)}>
                 <img src="/images/icons/close.png" alt="Close" />
-            </FeatureClose>
+            </FeatureClose>  
         </Content>
         <Group margin="30 0" flexDirection="row" alignItem="center">
             <Maturity rating={itemFeature.maturity}>
