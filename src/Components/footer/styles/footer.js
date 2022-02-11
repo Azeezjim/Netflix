e9bd3f8b147d85915b561b55d59d3dcbcdf6ffcf
Azeezjim <azeexjim@gmail.com>
2022-02-11 09:08:00 -0800

    7th  commit

--- src/Components/footer/styles/footer.js
+++ src/Components/footer/styles/footer.js
@@ -1,40 +1,37 @@
-import styled from 'styled-components';
-// import styles from 'styled-components';
+import styled from 'styled-components/macro';
 
 export const Container = styled.div`
   display: flex;
-  padding: 70px 50px;
+  padding: 70px 0;
   margin: auto;
   max-width: 1000px;
   flex-direction: column;
-  text-align: center;
 
-  @media (max-width) {
+  @media (max-width: 1000px) {
     padding: 70px 30px;
   }
 `;
 
 export const Column = styled.div`
   display: flex;
-  justify-content: space-around;
-  /* flex-direction: column; */
+  flex-direction: column;
   text-align: left;
 `;
 
 export const Row = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-full, minmax(230px ifr));
+  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
   grid-gap: 15px;
 
   @media (max-width: 1000px) {
-    grid-template-columns: repeat(150px ifr);
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
   }
 `;
 
 export const Link = styled.a`
   color: #757575;
   margin-bottom: 20px;
-  font-size: 14px;
+  font-size: 13px;
   text-decoration: none;
 `;
 
@@ -50,7 +47,7 @@ export const Text = styled.p`
   margin-bottom: 40px;
 `;
 
-export const Break = styled.p`
+export const Break = styled.div`
   flex-basis: 100%;
   height: 0;
 `;
